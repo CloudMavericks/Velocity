c3fc3c8b2b3e58f17cf64a6917046413c258c426
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-10-25 05:50:07 +0530

    Update LoginEndpoints

--- src/Velocity.Backend/Endpoints/LoginEndpoints.cs
+++ src/Velocity.Backend/Endpoints/LoginEndpoints.cs
@@ -14,25 +14,25 @@ internal static class LoginEndpoints
 {
     public static HttpResult Login(LoginRequest loginRequest, TokenConfiguration tokenConfiguration)
     {
-        if(loginRequest.UserName == "admin" && loginRequest.Password == "admin")
+        if (loginRequest.UserName != "admin" || loginRequest.Password != "admin")
         {
-            var claims = new List<Claim>
-            {
-                new ("Name", loginRequest.UserName),
-                new ("FullName", "Administrator"),
-            };
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(5),
-                signingCredentials: new SigningCredentials(
-                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfiguration.Secret)),
-                    SecurityAlgorithms.HmacSha256
-                )
-            );
-            var jwtHandler = new JwtSecurityTokenHandler();
-            var tokenString = jwtHandler.WriteToken(token);
-            return Results.Ok(Result<LoginResponse>.Success(new LoginResponse(tokenString)));
+            return Results.Unauthorized();
         }
-        return Results.Unauthorized();
+        var claims = new List<Claim>
+        {
+            new ("Name", loginRequest.UserName),
+            new ("FullName", "Administrator"),
+        };
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(5),
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenConfiguration.Secret)),
+                SecurityAlgorithms.HmacSha256
+            )
+        );
+        var jwtHandler = new JwtSecurityTokenHandler();
+        var tokenString = jwtHandler.WriteToken(token);
+        return Results.Ok(Result<LoginResponse>.Success(new LoginResponse(tokenString)));
     }
 }
