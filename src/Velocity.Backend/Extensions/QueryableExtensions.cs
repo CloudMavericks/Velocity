a5dd223041e0c9794d4972fcd2181793bfcb2170
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-10-30 17:34:39 +0530

    New QueryableExtensions

---
+++ src/Velocity.Backend/Extensions/QueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Velocity.Backend.Specifications;
+using Velocity.Shared.Contracts;
+
+namespace Velocity.Backend.Extensions;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> spec) where T : class, IEntity
+    {
+        var queryableResultWithIncludes = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+        var secondaryResult = spec.IncludeStrings.Aggregate(queryableResultWithIncludes, (current, include) => current.Include(include));
+        return secondaryResult.Where(spec.FilterCondition);
+    }
+}
