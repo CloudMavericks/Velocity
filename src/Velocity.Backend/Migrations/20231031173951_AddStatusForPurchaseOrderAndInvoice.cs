d4ab68755bb1703b4a1db0b80b4da5c982208d3c
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-10-31 23:29:25 +0530

    Generate database migrations for updated entities

---
+++ src/Velocity.Backend/Migrations/20231031173951_AddStatusForPurchaseOrderAndInvoice.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Velocity.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusForPurchaseOrderAndInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "PurchaseOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "PurchaseInvoices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "PurchaseInvoices");
+        }
+    }
+}
